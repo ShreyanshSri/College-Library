3a7b0db5aae5ec7acb348f120c271789e719ad50
Shreyansh <Shreyanshsri2812@gmail.com>
2022-06-20 23:11:39 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,54 @@
+console.log(`This is index.js`);
+
+function Book(name,author,type)
+{
+    this.name = name;
+    this.author = author;
+    this.type = type;
+}
+
+// Display constructor
+function Display()
+{
+
+}
+
+// Add methods to display prototype
+
+
+//Add submit event listener to libraryForm
+
+let libraryForm = document.getElementById('libraryForm');
+libraryForm.addEventListener('submit' , libraryFormSubmit);
+
+function libraryFormSubmit(e)
+{
+    console.log('You have submitted library form');
+    let name = document.getElementById('bookName').value; 
+    let author = document.getElementById('author').value;
+    let type;
+    let fiction = document.getElementById('fiction');
+    let programming = document.getElementById('programming');
+    let cooking = document.getElementById('cooking');
+
+    if(fiction.checked)
+    {
+        type = fiction.value;
+    }
+
+    else if(programming.checked)
+    {
+        type = programming.value;
+    }
+
+    else if(cooking.checked)
+    {
+        type = cooking.value;
+    }
+
+    let book = new Book(name,author,type);
+    console.log(book)
+
+    
+    e.preventDefault();
+}
